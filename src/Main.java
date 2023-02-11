efe6292a9b4bcd0a92f526d7b95db42473bc39f1
Kirill Igorevich <k.beketoff@yandex.ru>
2023-02-11 13:57:10 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-       int price = 210000;
-       int bonus = 20;
+        int price = 210000;
+        int bonus = 20;
         System.out.println(price / bonus);
-        int summaBonusa = 10500;
+        int amountBonus = 10500;
     }
 }
