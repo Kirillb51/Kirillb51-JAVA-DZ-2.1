f006175082ee23c02abc0e7f91f18068e423ae49
Kirill Igorevich <k.beketoff@yandex.ru>
2023-02-19 20:06:21 +0300

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 210000;
-        int bonus = 20;
-        int miles;
-        if (price < 0) {
-            System.out.println(0);
-        } else {
-            miles = price / bonus;
-            System.out.println("Количество миль составило:" + miles);
-
-        }
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
